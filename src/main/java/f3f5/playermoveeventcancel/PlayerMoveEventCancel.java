02ecdb55778e94c07986d04465a69a743147a79b
Misha <108226177+F3F5@users.noreply.github.com>
2023-01-26 11:09:57 +0400

    1.0

---
+++ src/main/java/f3f5/playermoveeventcancel/PlayerMoveEventCancel.java
@@ -0,0 +1,44 @@
+package f3f5.playermoveeventcancel;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerTeleportEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PlayerMoveEventCancel extends JavaPlugin implements Listener {
+    @Override
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onMove(PlayerMoveEvent event) {
+        Player player = event.getPlayer();
+        if (player.getWorld().getName().toLowerCase().contains("world")) {
+            if(player.getLocation().getBlockY() < 900){
+                player.teleport(new Location(player.getWorld(), 0, 1000, 0));
+            }
+//            player.setWalkSpeed(0F);
+            player.setFlySpeed(0F);
+            player.setAllowFlight(true);
+            player.setFlying(true);
+        }
+    }
+    @EventHandler
+    public void onTp(PlayerTeleportEvent event) {
+        Player player = event.getPlayer();
+        player.setHealth(player.getMaxHealth());
+//        player.setWalkSpeed(0.2F);
+    }
+        @EventHandler
+        public void onDamage(EntityDamageByEntityEvent event) {
+            if(event.getEntity().getWorld().getName().toLowerCase().contains("world")) {
+                event.setCancelled(true);
+            }
+        }
+}
